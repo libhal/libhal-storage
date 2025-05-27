c1c9a6f287f2c78518c5df6fd55a86fcc73c19af
Khalil Estell <khalil.estell@sjsu.edu>
2025-05-27 08:49:34 -0700

    :arrow_up: libhal-bootstrap/[^4.2.1] (#11)

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Khalil Estell
+# Copyright 2024 - 2025 Khalil Estell and the libhal contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ from conan import ConanFile
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    python_requires = "libhal-bootstrap/[^4.0.0]"
+    python_requires = "libhal-bootstrap/[^4.2.1]"
     python_requires_extend = "libhal-bootstrap.library_test_package"
 
     def requirements(self):
